567881ed06172922659a3dd30636213e16cdad00
ahall <ahall@ctream>
2020-09-08 21:45:09 +0100

    refactoring

--- mud-server.go
+++ mud-server.go
@@ -13,6 +13,8 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var database, _ = sql.Open("sqlite3", "./mud-database.db")
+
 func askQuestion(c net.Conn, q string, n string, y string) {
 	c.Write([]byte(string(q)))
 	for {
@@ -47,7 +49,6 @@ func createUser(c net.Conn, q string) string {
 		if username == "" {
 			c.Write([]byte(string("You need to enter a username: ")))
 		} else {
-			database, _ := sql.Open("sqlite3", "./mud-database.db")
 			_, err := database.Exec("INSERT OR FAIL INTO users (username, score, room, weapon) VALUES (?, ?, ?, ?)", username, 0, 1, 1)
 			if err != nil {
 				c.Write([]byte(string("Welcome back " + username + "\n")))
@@ -62,7 +63,6 @@ func createUser(c net.Conn, q string) string {
 }
 
 func enterRoom(c net.Conn, username string, room int) {
-	database, _ := sql.Open("sqlite3", "./mud-database.db")
 	_, err := database.Exec("UPDATE users SET room = ? WHERE username = ?", room, username)
 	if err !=nil {
 		fmt.Println(err)
@@ -83,34 +83,19 @@ func handleCommands(c net.Conn, username string, room int) {
 		}
 
 		text := strings.TrimSpace(string(netData))
-		database, _ := sql.Open("sqlite3", "./mud-database.db")
-		var result int
 		if text == "" {
 			c.Write([]byte(string("You need to enter a command: ")))
-		} else if text == "n" {
-			err = database.QueryRow("SELECT n FROM room WHERE id = ?", room).Scan(&result)
+		} else if ( text == "n" || text == "e" || text == "s" || text == "w" ) {
+			query := fmt.Sprintf("SELECT %v FROM room WHERE id = ?", text)
+			var result int
+			err = database.QueryRow(query, room).Scan(&result)
 			if err != nil {
 				log.Fatal(err)
 			}
 			enterRoom(c, username, result)
-		} else if text == "e" {
-			err = database.QueryRow("SELECT e FROM room WHERE id = ?", room).Scan(&result)
-			if err != nil {
-				log.Fatal(err)
-			}
-			enterRoom(c, username, result)
-		} else if text == "s" {
-			err = database.QueryRow("SELECT s FROM room WHERE id = ?", room).Scan(&result)
-			if err != nil {
-				log.Fatal(err)
-			}
-			enterRoom(c, username, result)
-		} else if text == "w" {
-			err = database.QueryRow("SELECT w FROM room WHERE id = ?", room).Scan(&result)
-			if err != nil {
-				log.Fatal(err)
-			}
-			enterRoom(c, username, result)
+		} else if text == "QUIT" {
+			c.Write([]byte(string("Thanks for playing " + username + " !!\n")))
+			os.Exit(0)
 		}
 	}
 }
@@ -120,8 +105,10 @@ func handleConnection(c net.Conn) {
 	askQuestion(c, "Welcome to FlexMUD dare you enter...(y/n): ", "Goodbye weakling.\n", "Good luck !!\n")
 	// get user details
 	username := createUser(c, "Please enter you username (new users will be created / existing users will be loaded): ")
-	// enter the map
-	enterRoom(c, username, 1)
+	// enter the map at last location
+	var room int
+	database.QueryRow("SELECT room FROM users WHERE username = ?", username).Scan(&room)
+	enterRoom(c, username, room)
 	for {
 		netData, err := bufio.NewReader(c).ReadString('\n')
 		if err != nil {
